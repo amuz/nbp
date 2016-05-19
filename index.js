0861deaf1bb541fb0afa95218198016edc266d20
muzelengaa <arthur.muzelenga@news.com.au>
2016-05-19 17:42:42 +1000

    nbp fileloader added

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express'),
+	fs = require('fs');
+var botObj = {};
+
+botObj.app = express();
+
+botObj.commandList = {
+	loadserver1: "think server restart",
+	loadserver2: "landingpage.sh batch file load"
+}
+
+botObj.loadCommand = function (res, command, req) {
+	res.send("queryparam2 " + botObj.commandList[command]);
+
+	botObj.updateCommandFile(res, botObj.commandList[command], req);
+}
+
+// app.get('/', function (req, res) {
+// 	res.json({ user: 'tobi' });
+// });
+
+botObj.app.get('/bot', function (req, res) {
+	//res.json({ user: 'tobi' });
+	//res.send('queryparam ' + req.query.loader);
+	botObj.loadCommand (res, req.query.loader, req);
+});
+
+botObj.app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+botObj.updateCommandFile = function (res, command, req) {
+	//__dirname + '/botcommandlist/data.txt'
+	fs.appendFile(__dirname + '/botcommandlist/data.txt', '\r\n' + command , function (err) {
+
+	});
+
+
+	// console.log(__dirname);
+ //    var body = '';
+ //    filePath = __dirname + '/botcommandlist/data.txt';
+ //    req.on('data', function(data) {
+ //        body += data;
+ //        console.log(data);
+ //    });
+
+ //    req.on('end', function (){
+ //        // fs.appendFile(filePath, body, function() {
+ //            res.end();
+ //        // });
+ //    });
+}
